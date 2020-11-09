55de34d380b3268f62df9e07cdb6309d21b2425d
aronrweston <aron@firststepdigital.com.au>
2020-11-09 22:45:18 +1100

    integrated localstorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,128 @@
 
-//Swim App 
-//TODO'
-//1. Get all input values to submit to the console
-//2. 
+const submit = document.querySelector("#submit");
+const form = document.querySelector("#swim-data");
+const output = document.querySelector("#output");
+const del = document.querySelector(".remove");
 
-(function () {
-    let submit = document.querySelector("#submit");
-    submit.addEventListener("click", function (e) {
-        e.preventDefault();
-        //Def lets
-        let today = new Date();
-        let laps = document.querySelector("#laps").value;
-        let notes = document.querySelector("#feeling").value; 
-        let output = document.querySelector("#output");
+//Event listeners
+eventListeners()
+function eventListeners() {
+    submit.addEventListener("click", addSwimData);
+    document.addEventListener("DOMContentLoaded", getSwimData);
+    del.addEventListener("click", deleteSwimData);
+}
 
+//Date
+const newDate = new Date();
+let month = newDate.getMonth() + 1;
+let day = newDate.getDate();
+let year = newDate.getFullYear();
+
+//Variables
+const date = `${day}/${month}/${year}`;
+const laps = document.querySelector("#laps");
+const notes = document.querySelector("#notes");
+
+
+function storeSwimData(a) {
+    let data;
+    if (localStorage.getItem("data") === null) {
+        data = [];
+    } else {
+      data = JSON.parse(localStorage.getItem("data"));
+    }
+    data.push(a);
+    localStorage.setItem('data', JSON.stringify(data));
+}
+
+function addSwimData(e) {
+    e.preventDefault();
+    //Remove 
+    let remove = document.createElement('td');
+    remove.className = 'data-remove';
+
+    let link = document.createElement('a')
+    link.setAttribute("href", "#");
+    link.className = 'remove';
+    link.appendChild(document.createTextNode("Delete"));
+
+    remove.appendChild(link);
+    
+    // Date
+    let dateTD = document.createElement('td');
+        dateTD.className ='data-date';
+        dateTD.appendChild(document.createTextNode(date));
+
+    //Laps
+    let lapsTD = document.createElement('td');
+        lapsTD.className ='data-laps';
+        lapsTD.appendChild(document.createTextNode(laps.value));
+    
+    //Notes
+    let notesTD = document.createElement('td');
+        notesTD.className ='data-notes';
+        notesTD.appendChild(document.createTextNode(notes.value));
+
+    //Table row
+    const tr = document.createElement('tr');
+    tr.className = 'data-content';
+    tr.appendChild(remove)
+    tr.appendChild(dateTD);
+    tr.appendChild(lapsTD);
+    tr.appendChild(notesTD);
+
+
+    console.log(tr);
+    
+    output.appendChild(tr);
+
+    let input = [date, laps.value, notes.value];
+    //     "Date": date,
+    //     "Laps": laps.value,
+    //     "Notes": notes.value
+    
         
-        let table = output.innerHTML += `
-        <tr class="data-content">
-            <td class="data-date">${today}</td>
-            <td class="data-laps">${laps}</td> 
-            <td class="data-notes">${notes}</td>
-        </tr>
-        `
-        output.firstElementChild.innerHTML = table;
+    storeSwimData(input);
+}
 
 
+function getSwimData() {
+    let data;
+    if (localStorage.getItem("data") === null) {
+        data = [];
+    } else {
+      data = JSON.parse(localStorage.getItem("data"));
+    }
+    //loop through the tasks
+    data.forEach(function (data) {
 
-    }, false);
-})();
+        // Date
+        let dateTD = document.createElement('td');
+        dateTD.className ='data-date';
+        dateTD.appendChild(document.createTextNode(date));
+
+        //Laps
+        let lapsTD = document.createElement('td');
+        lapsTD.className ='data-laps';
+        lapsTD.appendChild(document.createTextNode(laps));
+        
+        //Notes
+        let notesTD = document.createElement('td');
+        notesTD.className ='data-notes';
+        notesTD.appendChild(document.createTextNode(notes));
+
+        //Table row
+        const tr = document.createElement('tr');
+        tr.className = 'data-content';
+        tr.appendChild(dateTD);
+        tr.appendChild(lapsTD);
+        tr.appendChild(notesTD);
+        output.appendChild(tr);
+    });
+}
+
+
+function deleteSwimData() {
+    
+    
+}
